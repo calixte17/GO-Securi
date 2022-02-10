ce734ff908ad332f180af8892ad2db5ca14a62b5
Amaury <mr.amaury@hotmail.fr>
2022-02-10 10:31:09 +0100

    [ADD] Trie par alphabet + equipement par catégories

---
+++ java/Equipement.java
@@ -0,0 +1,9 @@
+
+public class Equipement {
+	String id ;
+	String Name;
+	public Equipement(String id , String Name) {
+		this.id = id ;
+		this.Name=Name;
+	}
+}
--- java/Main.java
+++ java/Main.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
@@ -10,38 +11,47 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
         String str = "";
         List<String> lAgent = new Vector<String>();
-        List<String> lEquipement = new Vector<String>();
+        List<Equipement> lEquipement = new Vector<Equipement>();
         List<String> lFiche = new Vector<String>();
         //File doc = new File("test.txt");
-        File dir = new File("C:\\Users\\a.dupont\\Documents\\GO-Securi\\java\\file");
+        File dir = new File("java\\file");
         File[] FileSearch = dir.listFiles(new FilenameFilter() {
             public boolean accept(File dir, String name) {
                 return name.endsWith("txt");
         }  });
         
+        File liste = new File("java\\file\\liste.txt");
+        Scanner listetxt = new Scanner(liste);
+            str = "";
+            while (listetxt.hasNextLine()){
+                str += str == "" ? listetxt.nextLine() : "," + listetxt.nextLine();
+            }
+            
+            String[] parts = str.split(",");
+          
+            for (String part : parts) {
+                String[] equipement = part.split("\t");
+                String cat = equipement[0];
+               
+               for(String equip : equipement) {
+            	   if(equip != cat) {
+            	   Equipement stuff = new Equipement(cat,equip);
+            	   lEquipement.add(stuff);
+            	   }
+               }
+            }
+            listetxt.close();
         for (File doc : FileSearch) {
 
             Scanner obj = new Scanner(doc);
-            
-            if (doc.getPath().contains("equipement.txt")){
-                str = "";
-                while (obj.hasNextLine()){
-                    str += str == "" ? obj.nextLine() : "," + obj.nextLine();
-                }
-                
-                String[] parts = str.split(",");
-                for (String part : parts) {
-                    lEquipement.add(part);
-                }
-            }
-
+             if (doc.getPath().contains("liste.txt")){}
             else if (doc.getPath().contains("staff.txt")){
                 str = "";
                 while (obj.hasNextLine()){
                     str += str == "" ? obj.nextLine() : "," + obj.nextLine();
                 }
                 
-                String[] parts = str.split(",");
+                 parts = str.split(",");
                 for (String part : parts) {
                     lAgent.add(part);
                 }
@@ -52,12 +62,12 @@ public class Main {
                     str += str == "" ? obj.nextLine() : "," + obj.nextLine();
                 }
                 
-                String[] parts = str.split(",");
+                 parts = str.split(",");
                 for (String part : parts) {
                     lFiche.add(part);
                 }
                 String NameAgent=parts[1].toLowerCase().charAt(0)+parts[0].toLowerCase();
-                PrintWriter writer = new PrintWriter("C:\\Users\\a.dupont\\Documents\\GO-Securi\\java\\"+NameAgent+".html");
+                PrintWriter writer = new PrintWriter("java\\"+NameAgent+".html");
                 lFiche.remove(parts[0]);
                 lFiche.remove(parts[1]);
                 lFiche.remove(parts[2]);
@@ -69,7 +79,7 @@ public class Main {
                 writer.println("<div class='boxName'>");
                 writer.println("<a>"+parts[0]+"<a>");
                 writer.println("</div>");
-                writer.println("<img class='imgAgent' src='img\\"+NameAgent+".png'/>");
+                writer.println("<img class='imgAgent' style='padding:10px' src='img\\"+NameAgent+".jpg'/>");
                 writer.println("</div>");
                 //body
                 writer.println("<div class='container listeEquipement'>");
@@ -77,7 +87,12 @@ public class Main {
               
                 writer.println("<h2>Equipements </h2>");
                 for (String ficheAgent : lFiche){
-                    writer.println("<div class='Equipement'><a>"+ficheAgent+"<a></div>");
+                	for(Equipement equip : lEquipement){
+                		if(equip.id.contains(ficheAgent)) {
+                			writer.println("<div class='Equipement'><a>"+equip.Name+"<a></div>");
+                		}
+                	}
+                	
                 }
               
                 writer.println("</div>");
@@ -87,6 +102,7 @@ public class Main {
                 lFiche.removeAll(lFiche);
             }
         }
+        java.util.Collections.sort(lAgent);
         // génération html
         PrintWriter writer = new PrintWriter("C:\\Users\\a.dupont\\Documents\\GO-Securi\\java\\index.html");
         //header
@@ -111,5 +127,7 @@ public class Main {
         writer.close();
     }
 }
+
+
 
 
